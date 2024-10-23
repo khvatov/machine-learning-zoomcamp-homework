dec1149a12bed9b3e08494cbd520d45831c4b02b
Alex Khvatov <akhvatov@us.imshealth.com>
2024-10-23 14:27:56 -0400

    Adds python files for the service

---
+++ 05-deployment/predict.py
@@ -0,0 +1,44 @@
+import pickle
+
+input_file = 'model_C=1.0.bin'
+
+with open(input_file, 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+
+
+customer = {
+'gender':'female',
+    'seniorcitizen':0,
+    'partner':'yes',
+    'dependents':'no',
+    'phoneservice':'no',
+    'multiplelines':'no_phone_service',
+    'internetservice':'dsl',
+    'onlinesecurity':'no',
+    'onlinebackup':'yes',
+    'deviceprotection':'no',
+    'techsupport':'no',
+    'streamingtv':'no',
+    'streamingmovies':'no',
+    'contract':'month-to-month',
+    'paperlessbilling':'yes',
+    'paymentmethod':'electronic_check',
+    'tenure':1,
+    'monthlycharges':29.85,
+    'totalcharges':29.85
+    
+}
+
+
+
+X=dv.transform([customer])
+
+
+y_pred = model.predict_proba(X)[0,1]
+print(f'Customer churn probability: {y_pred:.3f}')  # 0.000
+
+
+
+
+
